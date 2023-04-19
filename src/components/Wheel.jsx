f5a52faa636675798c0260d77842739d048d4b87
roodhouse <rughjm@gmail.com>
2023-04-19 14:52:39 -0500

    small edits

--- src/components/Wheel.jsx
+++ src/components/Wheel.jsx
@@ -1,7 +1,7 @@
 import { React, useState } from 'react';
 import { Link } from 'react-router-dom';
 import { TextField } from '@mui/material';
-import {getColors} from './colors.js';
+import { getColors, colors } from './colors.js';
 import List from './List.jsx';
 
 function Wheel() {
@@ -11,8 +11,7 @@ function Wheel() {
         var lowerCase = e.target.value.toLowerCase();
         setInputText(lowerCase);
     }
-    const colors = getColors();
-
+    
   return (
     <div name='wheel' className='flex flex-col justify-center max-w-[1300px] mx-auto px-8  h-full pt-[15rem] mb-8'>
         <h1 className='mb-2'>Color scheme search</h1>
