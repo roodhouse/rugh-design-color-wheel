b364f2e2b9118611dbb5eb459104b8332828968d
roodhouse <rughjm@gmail.com>
2023-04-19 12:57:26 -0500

    format code

--- src/components/color/ColorNav.jsx
+++ src/components/color/ColorNav.jsx
@@ -2,9 +2,6 @@ import * as React from 'react';
 import { useState } from 'react';
 import { useParams, Link } from 'react-router-dom';
 import { getColor, colors } from '../colors';
-import Tooltip from '@mui/material/Tooltip';
-import Fade from '@mui/material/Fade';
-import Zoom from '@mui/material/Zoom';
 
 function ColorNav() {
 
@@ -58,50 +55,77 @@ function ColorNav() {
     }
   }
 
-  console.log(colors[nextIndex].name)
-
   return (
-    <div className='max-w-[1000px] w-full flex flex-row justify-around mx-auto pt-10'>
-        <div className='w-[200px] text-center whitespace-nowrap'>
-
-            <Link onMouseOver={handlePrevMouseOver} onMouseOut={handlePrevMouseOut} to={`../color/${thePreviousColor()}`}>
-                {prevIsHovering && (
-                    <div className={thePreviousColor() === 0 ? "hidden" : "hover:text-ellipsis hover:whitespace-nowrap hover:overflow-hidden block bg-white text-[#676766] border-2 px-6 py-3 hover:bg-[#E5C1C1] hover:border-[#E5C1C1] hover:text-white"}>
+    <div className="max-w-[1000px] w-full flex flex-row justify-around mx-auto pt-10">
+      <div className="w-[200px] text-center whitespace-nowrap">
+        <Link
+          onMouseOver={handlePrevMouseOver}
+          onMouseOut={handlePrevMouseOut}
+          to={`../color/${thePreviousColor()}`}
+        >
+          {prevIsHovering && (
+            <div
+              className={
+                thePreviousColor() === 0
+                  ? "hidden"
+                  : "hover:text-ellipsis hover:whitespace-nowrap hover:overflow-hidden block bg-white text-[#676766] border-2 px-6 py-3 hover:bg-[#E5C1C1] hover:border-[#E5C1C1] hover:text-white"
+              }
+            >
               {colors[prevIndex].name}
-          </div>      
-                )}
-                {!prevIsHovering && (
-          <div className={thePreviousColor() === 0 ? "hidden" : "block bg-white text-[#676766] border-2 px-6 py-3 hover:bg-[#E5C1C1] hover:border-[#E5C1C1] hover:text-white"}>
+            </div>
+          )}
+          {!prevIsHovering && (
+            <div
+              className={
+                thePreviousColor() === 0
+                  ? "hidden"
+                  : "block bg-white text-[#676766] border-2 px-6 py-3 hover:bg-[#E5C1C1] hover:border-[#E5C1C1] hover:text-white"
+              }
+            >
               Previous Color
+            </div>
+          )}
+        </Link>
+      </div>
+      <div className="w-[200px] text-center">
+        <Link to={`../`}>
+          <div className="block bg-white text-[#676766] border-2 px-6 py-3 hover:bg-[#E5C1C1] hover:border-[#E5C1C1] hover:text-white">
+            Back to color wheel
           </div>
-                )}
-            </Link>
-        </div>
-        <div className='w-[200px] text-center'>
-
-            <Link to={`../`}>
-          <div className='block bg-white text-[#676766] border-2 px-6 py-3 hover:bg-[#E5C1C1] hover:border-[#E5C1C1] hover:text-white'>
-                Back to color wheel
-          </div>
-                </Link>
-        </div>
-        <div className='w-[200px] text-center whitespace-nowrap'>
-
-            <Link onMouseOver={handleNextMouseOver} onMouseOut={handleNextMouseOut} to={`../color/${theNextColor()}`}>
-            {nextIsHovering && (
-                    <div className={theNextColor() === 0 ? "hidden" : "hover:text-ellipsis hover:whitespace-nowrap hover:overflow-hidden block bg-white text-[#676766] border-2 px-6 py-3 hover:bg-[#E5C1C1] hover:border-[#E5C1C1] hover:text-white"}>
+        </Link>
+      </div>
+      <div className="w-[200px] text-center whitespace-nowrap">
+        <Link
+          onMouseOver={handleNextMouseOver}
+          onMouseOut={handleNextMouseOut}
+          to={`../color/${theNextColor()}`}
+        >
+          {nextIsHovering && (
+            <div
+              className={
+                theNextColor() === 0
+                  ? "hidden"
+                  : "hover:text-ellipsis hover:whitespace-nowrap hover:overflow-hidden block bg-white text-[#676766] border-2 px-6 py-3 hover:bg-[#E5C1C1] hover:border-[#E5C1C1] hover:text-white"
+              }
+            >
               {colors[nextIndex].name}
-          </div>      
-                )}
-                {!nextIsHovering && (
-          <div className={theNextColor() === 0 ? "hidden" : "block bg-white text-[#676766] border-2 px-6 py-3 hover:bg-[#E5C1C1] hover:border-[#E5C1C1] hover:text-white"}>
+            </div>
+          )}
+          {!nextIsHovering && (
+            <div
+              className={
+                theNextColor() === 0
+                  ? "hidden"
+                  : "block bg-white text-[#676766] border-2 px-6 py-3 hover:bg-[#E5C1C1] hover:border-[#E5C1C1] hover:text-white"
+              }
+            >
               Next Color
-          </div>
-                )}
-            </Link>
-        </div>
-        </div>
-  )
+            </div>
+          )}
+        </Link>
+      </div>
+    </div>
+  );
 }
 
 export default ColorNav
